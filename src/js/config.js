12875c17068994f92b67e02d5e12afe7f1e9fae3
angelo-cantoli <angelocantoli2003@gmail.com>
2024-03-05 21:07:53 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '25e6e3d4-1c0e-4fb8-b0e0-62223249f827';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,18 @@
+import View from './view';
+import previewView from './previewView';
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage =
+    'No recipe found for your query! Please try another one! 💥💥';
+  _message = '';
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+export default new ResultsView();
