a1a1d656f5fd9889be9e3b9aa17ce4ca7b5a78fd
angelo-cantoli <angelocantoli2003@gmail.com>
2024-03-05 21:19:29 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('ciaoooo');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,7 +133,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
+  newFeature();
 };
 
 init();
